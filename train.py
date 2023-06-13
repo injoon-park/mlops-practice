da8a2247623c7583fd50cd9af6c1677e8fcde5ee
injoon-park <131101655+injoon-park@users.noreply.github.com>
2023-06-13 05:14:47 +0000

    Commit batch_pred

--- train.py
+++ train.py
@@ -1,5 +1,3 @@
-# TODO: 적절한 위치에 맞는 수준으로 로그 출력되도록 코드 작성
-
 # sourcery skip: raise-specific-error
 import os
 import sys
@@ -24,7 +22,8 @@ from src.common.utils import get_param_set
 from src.preprocess import preprocess_pipeline
 
 # 로그 들어갈 위치
-# TODO: 로그를 정해진 로그 경로에 logs.log로 저장하도록 설정
+# 로그를 정해진 로그 경로에 logs.log로 저장하도록 설정
+logger = set_logger(os.path.join(LOG_FILEPATH, "logs.log"))
 
 sys.excepthook = handle_exception
 warnings.filterwarnings(action="ignore")
@@ -32,6 +31,7 @@ warnings.filterwarnings(action="ignore")
 
 if __name__ == "__main__":
     train_df = pd.read_csv(os.path.join(DATA_PATH, "house_rent_train.csv"))
+    logger.info("Load data...")
 
     _X = train_df.drop(["rent", "area_locality", "posted_on"], axis=1)
     y = np.log1p(train_df["rent"])
@@ -47,6 +47,7 @@ if __name__ == "__main__":
         index=False,
     )
 
+    logger.info("Set candidates for hyper-params...")
     params_candidates = {
         "learning_rate": [0.01, 0.05, 0.1],
         "max_depth": [3, 4, 5, 6],
@@ -56,7 +57,8 @@ if __name__ == "__main__":
     param_set = get_param_set(params=params_candidates)
 
     # Set experiment name for mlflow
-    experiment_name = "new_experiment"
+    logger.debug("Set the experiment name for MLflow run")
+    experiment_name = "new_experiment_2"
     mlflow.set_experiment(experiment_name=experiment_name)
     mlflow.set_tracking_uri("./mlruns")
 
@@ -72,6 +74,8 @@ if __name__ == "__main__":
 
             # get evaluations scores
             score_cv = rmse_cv_score(regr, X, y)
+
+            logger.info("Cross-validation RMSE score for Run")
 
             name = regr.__class__.__name__
             mlflow.set_tag("estimator_name", name)
@@ -105,10 +109,11 @@ if __name__ == "__main__":
 
     best_run = mlflow.get_run(best_run_df.at[0, "run_id"])
     best_params = best_run.data.params
+    logger.info(f"Best hyper-parameter: {best_params}")
 
     best_model_uri = f"{best_run.info.artifact_uri}/model"
 
-    # TODO: 베스트 모델을 아티팩트 폴더에 복사
+    # 베스트 모델을 아티팩트 폴더에 복사
     copy_tree(best_model_uri.replace("file://", ""), ARTIFACT_PATH)
 
     # BentoML에 모델 저장
